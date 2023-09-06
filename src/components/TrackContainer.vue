73702446518d67adb757fb2d4adc462b13c412db
ngentili <54852718+ngentili@users.noreply.github.com>
2023-09-06 15:59:21 -0400

    begin toolbar

---
+++ src/components/TrackContainer.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import TrackControl from './TrackControl.vue'
+import TrackTimeline from './TrackTimeline.vue'
+</script>
+
+<template>
+    <TrackControl />
+    <TrackTimeline />
+</template>
---
+++ src/components/TrackControl.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+defineProps<{
+    tempo: number
+    beatsPerMeasure: number
+    beatDuration: number
+    swing: number
+    isPlaying: boolean
+}>()
+
+const emit = defineEmits<{
+    tempoChange: [value: number]
+    beatsPerMeasureChange: [value: number]
+    beatDurationChange: [value: number]
+    swingChange: [value: number]
+    playingChange: [value: boolean]
+}>()
+</script>
+
+<template>
+    <div>
+        <input type="number" placeholder="tempo"
+            @change="e => emit('tempoChange', (e.target as HTMLInputElement).valueAsNumber)">
+        <div>{{ tempo }}</div>
+    </div>
+    <div>
+        <input type="number" placeholder="beats per measure"
+            @change="e => emit('beatsPerMeasureChange', (e.target as HTMLInputElement).valueAsNumber)">
+        <div>{{ beatsPerMeasure }}</div>
+    </div>
+    <div>
+        <input type="number" placeholder="beat duration"
+            @change="e => emit('beatDurationChange', (e.target as HTMLInputElement).valueAsNumber)">
+        <div>{{ beatDuration }}</div>
+    </div>
+    <div>
+        <input type="number" placeholder="swing"
+            @change="e => emit('swingChange', (e.target as HTMLInputElement).valueAsNumber)">
+        <div>{{ swing }}</div>
+    </div>
+    <div>
+        <button @click="() => emit('playingChange', !isPlaying)">Play/Stop</button>
+        <div>{{ isPlaying }}</div>
+    </div>
+</template>
